cd8669db3b1468db17b250a6d6a7423f93f67446
Adam Marton Beke <65732867+BekeAtGithub@users.noreply.github.com>
2024-09-16 12:30:57 -0700

    Update network.tf

--- AKS/helm/nginxIngressController/network.tf
+++ AKS/helm/nginxIngressController/network.tf
@@ -1,4 +1,4 @@
-
+#has basic networking components like the resource group, virtual network, and subnet
 resource "azurerm_virtual_network" "vnet" {
   name                = var.vnet_name
   address_space       = var.vnet_address_space
