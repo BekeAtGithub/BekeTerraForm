4f4f78703ac2522aad07757ea5da4f2b3ca4d236
Adam Marton Beke <65732867+BekeAtGithub@users.noreply.github.com>
2024-09-16 12:40:35 -0700

    Update network.tf

--- AKS/containers/envoy/network.tf
+++ AKS/containers/envoy/network.tf
@@ -1,4 +1,4 @@
-#This is basically the main.tf type file, has basic networking components like the resource group, virtual network, and subnet
+#basic networking components like the resource, virtual network, and subnet
 resource "azurerm_virtual_network" "vnet" {
   name                = var.vnet_name
   address_space       = var.vnet_address_space
