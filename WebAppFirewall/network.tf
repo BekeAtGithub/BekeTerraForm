5958caeeb340bc40247f92ebcddfad33c7cbd786
Adam Marton Beke <65732867+BekeAtGithub@users.noreply.github.com>
2024-09-16 12:41:44 -0700

    Update network.tf

--- WebAppFirewall/network.tf
+++ WebAppFirewall/network.tf
@@ -1,4 +1,4 @@
-#This is basically the main.tf type file, has basic networking components like the resource group, virtual network, and subnet
+#networking components; the resource group, virtual network, and subnet
 resource "azurerm_resource_group" "example" {
   name     = var.resource_group_name
   location = var.location
